863697b2fa547ff5ca7f267c1ffcd13850696c15
ShazaElali <shaza.el-ali@isae.edu.lb>
2021-05-21 11:55:17 +0300

    firt creation for files

--- deuxièmeEssai/B.java
+++ deuxièmeEssai/B.java
@@ -17,6 +17,7 @@ public class B extends A
     {
         // initialisation des variables d'instance
         x = 0;
+           System.out.println("Im  in class B extends A");    
     }
 
     /**
